c291c20109168c2ec56f0dd3b6a917a0741c0493
15138730370 <1037483576@qq.com>
2018-01-30 15:08:11 +0800

    update to save

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,3 @@
-export { ReactComponent } from './react.component';
+export { ReactComponent, KeyValue } from './react.component';
 export { ReactComponentSetting } from './react.component.setting';
 export { ReactCommonModule } from './common/common.module';
